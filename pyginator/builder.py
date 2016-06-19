e8f5b82d66cfe6ff4c5506868021bde3900d8c9a
Alexander Finn <alex@solu.co>
2016-06-19 14:02:12 +0300

    Multiple blocks per page support

--- pyginator/builder.py
+++ pyginator/builder.py
@@ -15,9 +15,37 @@ class Header(object):
         self.data_files = data.get("data", [])
 
 
+class Block(object):
+
+    def __init__(self, text):
+        self.name = 'body'
+        self.block_type = 'html'
+        lines = text.splitlines(False)
+        has_header = False
+        if lines[0].startswith('NAME:'):
+            self.name = lines[0].split(':')[1]
+            lines = lines[1:]
+            has_header = True
+        if lines[0].startswith('TYPE:'):
+            self.block_type = lines[0].split(':')[1].upper()
+            lines = lines[1:]
+            has_header = True
+
+        if has_header:
+            self.text = '\n'.join(lines)
+        else:
+            self.text = text
+
+    def render(self, context):
+        result = Template(self.text).render(**context)
+        if self.block_type == 'MD':
+            result = markdown.markdown(result)
+        return result
+
+
 class Builder(object):
 
-    HEADER_SEPARATOR = '===\n'
+    HEADER_SEPARATOR = '\n===\n'
 
     def __init__(self, configuration):
         self.configuration = configuration
@@ -46,19 +74,23 @@ class Builder(object):
 
 
     def render(self, file):
-        header, html = self.read_file(file)
+        header, blocks = self.read_file(file)
+        rendered = {}
+        context = self.get_context(header)
+        for block in blocks:
+            rendered[block.name] = block.render(context)
+        if not header.template:
+            return rendered.get('body', ''), header
+        else:
+            context.update(rendered)
+            template = self.jinja_env.get_template(header.template)
+            return template.render(**context), header
+
+    def get_context(self, header):
         context = header.fields
         context.update(self.get_data(header))
         context.update(self.configuration.fields)
-        body = Template(html.decode('utf-8')).render(**context)
-        if file.endswith('.md'):
-            body = markdown.markdown(body)
-        if not header.template:
-            return body, header
-        else:
-            context.update({'body': body})
-            template = self.jinja_env.get_template(header.template)
-            return template.render(**context), header
+        return context
 
     def get_data(self, header):
         result = {}
@@ -81,8 +113,8 @@ class Builder(object):
         f = open(os.path.join(self.configuration.sources_abs_path, file), 'r')
         is_header = True
         fc = f.read()
-        header, html = fc.split('===')
-        return Header(header), html
+        blocks = fc.split(HEADER_SEPARATOR)
+        return Header(blocks[0]), [Block(b) for b in blocks[1:]]
 
     def get_target_file(self, url):
         target = os.path.join(self.configuration.target_path, url)
---
+++ tests/test_block.py
@@ -0,0 +1,42 @@
+import context
+import unittest
+from pyginator.builder import Block
+
+
+class BlockTest(unittest.TestCase):
+
+    def test_simple_block(self):
+        text = 'This is the most simplistic block'
+        block = Block(text)
+        self.assertEqual(block.name, 'body')
+        self.assertEqual(block.block_type, 'html')
+        self.assertEqual(block.text, text)
+
+    def test_block_with_name(self):
+        name_text = 'NAME:test'
+        block_text = 'This is the most simplistic block'
+        text = '\n'.join((name_text, block_text))
+        block = Block(text)
+        self.assertEqual(block.name, 'test')
+        self.assertEqual(block.block_type, 'html')
+        self.assertEqual(block.text, block_text)
+
+    def test_block_with_name_and_type(self):
+        name_text = 'NAME:test'
+        type_text = 'TYPE:md'
+        block_text = 'This is the most simplistic block'
+        text = '\n'.join((name_text, type_text, block_text))
+        block = Block(text)
+        self.assertEqual(block.name, 'test')
+        self.assertEqual(block.block_type, 'MD')
+        self.assertEqual(block.text, block_text)
+
+    def test_block_multiline(self):
+        name_text = 'NAME:abc d e f'
+        type_text = 'TYPE:md'
+        block_text = 'This is the\nmultiline\nblock'
+        text = '\n'.join((name_text, type_text, block_text))
+        block = Block(text)
+        self.assertEqual(block.name, 'abc d e f')
+        self.assertEqual(block.block_type, 'MD')
+        self.assertEqual(block.text, block_text)
